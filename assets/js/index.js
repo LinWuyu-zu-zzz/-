2c3a69597530aa3563a79bcaa78802112a26c666
kiki <1604540911@qq.com>
2022-08-04 11:19:43 +0800

    公共部分完成

---
+++ assets/js/index.js
@@ -0,0 +1,46 @@
+const getUserInfo = () => {
+    $.ajax({
+        type: 'GET',
+        url: '/my/userinfo',
+        data: null,
+        success: res => {
+            console.log(res);
+            renderAvatar(res.data)
+        }
+    })
+}
+
+// 渲染用户头像
+const renderAvatar = (data) => {
+    // 获取用户名字
+    let name = data.nickname || data.username;
+    // 设置欢迎文本
+    $("#welcome").html(`欢迎 ${name}`);
+    // 按需渲染用户头像
+    if (data.user_pic !== null) {
+        // 渲染图片头像
+        $(".layui-nav-img").attr("src", data.user_pic).show();
+        $(".text-avatar").hide();
+    } else {
+        // 渲染文本头像
+        $(".layui-nav-img").hide();
+        let firstName = name[0].toUpperCase();
+        $(".text-avatar").html(firstName);
+    }
+};
+
+getUserInfo()
+
+// 退出登录
+$("#editBtn").click(() => {
+    layui.layer.confirm(
+        "确定退出登录？",
+        { icon: 3, title: "" },
+        function (index) {
+            // 清空本地存储里面的 token
+            localStorage.removeItem("token");
+            // 重新跳转到登录页面
+            location.href = "/login.html";
+        }
+    );
+});
