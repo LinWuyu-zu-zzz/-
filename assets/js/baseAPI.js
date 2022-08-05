b7b2f3d2d1344b3c57c7943a027fd364325f36d1
kiki <1604540911@qq.com>
2022-08-05 10:36:36 +0800

    文章类别管理功能完成

--- assets/js/baseAPI.js
+++ assets/js/baseAPI.js
@@ -2,7 +2,7 @@
 //在 `/assets/js` 目录中新建 `baseAPI.js`
 //调用 `$.ajaxPrefilter()` 函数，里面传递一个回调函数，回调函数里面有一个形成 `options`，这个形成里面就包含了这一次请求的相关信息
 
-const baseUrl = "http://www.liulongbin.top:3007"
+const baseUrl = "http://big-event-api-t.itheima.net"
 // 注意：每次调用 $.get() 或 $.post() 或 $.ajax() 的时候,会先调用 ajaxPrefilter 这个函数
 // 在这个函数中，可以拿到我们给Ajax提供的配置对象
 $.ajaxPrefilter((option) => {
